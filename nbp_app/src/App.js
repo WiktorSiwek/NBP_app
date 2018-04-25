a272dc929123c3216cd008d9278173f466357d3c
Siwek <wrse@gft.com>
2018-04-25 16:20:41 +0200

    Added user input

--- nbp_app/src/App.js
+++ nbp_app/src/App.js
@@ -9,7 +9,7 @@ class AppDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      table: [],
+      table: '',
     }
   }
 
@@ -18,28 +18,81 @@ class AppDashboard extends Component {
       method: 'GET',
     })
       .then(res => res.json())
+      .then(res => {console.log(res[0].rates[0])
+        return res}
+    )
       .then(res => {
+        console.log(res[0].rates)
+        // this.setState({
+        //   table: res[0].rates.map(r => (
+        //   <div>{r.currency}-{r.code}-{r.bid}-{r.ask} </div>
+        // ))
+        // })
         this.setState({
-          table: this.state.table.concat(res[0].rates)
+          table: res[0].rates.map(el => {
+            return(
+            <tbody>
+              <tr>
+                <td>{el.currency}</td>
+                <td>{el.code}</td>
+                <td>{el.bid}</td>
+                <td>{el.ask}</td>
+              </tr>
+            </tbody>
+            )
+          })
         })
-        console.log(this.state.table)
       })
   }
 
   render() {
     return (
       <div>
-        
+        {this.state.table}
       </div>
     )
   }
 }
 
+const Header = () => {
+  return (
+    <div>
+      <tbody style={{fontWeight: "bold"}}>
+        <tr>
+          <td>Nazwa waluty</td>
+          <td>Skrót</td>
+          <td>Cena sprzedaży</td>
+          <td>Cena kupna</td>
+        </tr>
+      </tbody> 
+    </div>
+  )
+}
+
+class ChooseCurrency extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="amountOfCurriencies">Ile kursów chcesz wyświetlić?</label>
+          <input id="amountOfCurriencies" type="text"/>
+          <br/>
+          <label htmlFor="whichCurrency">Jaka waluta?</label>
+          <input id="whichCurrency" type="text"/>
+        </form>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <AppDashboard />
+      <div>
+        <Header />
+        <AppDashboard />
+        <ChooseCurrency />
+      </div>
     );
   }
 }
