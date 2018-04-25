dab8fd9660005243a38894a509e5b7d10c31a7a4
Siwek <wrse@gft.com>
2018-04-25 12:33:46 +0200

    Implemented fetching data

--- nbp_app/src/App.js
+++ nbp_app/src/App.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API = 'http://api.nbp.pl/api/exchangerates/tables/C/?format=json';
+
+class AppDashboard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      table: [],
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(API, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          table: this.state.table.concat(res[0].rates)
+        })
+        console.log(this.state.table)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppDashboard />
     );
   }
 }
